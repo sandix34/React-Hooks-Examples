b146bf39209fd2a07c47be1c8d8c3be6c6c10dbf
sandrine <sandrinezocca@gmail.com>
2019-07-23 15:51:14 +0200

    Un useEffect() par fonctionnalité

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { directive } from '@babel/types';
 
 const PostList = () => {
 
     let [posts, setPosts] = useState([]);
+    let [counter, setCounter] = useState(0);
+    let [counterDisabled, setCounterDisabled] = useState(false);
 
     useEffect(() => {
         axios
@@ -12,10 +13,24 @@ const PostList = () => {
             .then(result => {
                 setPosts(result.data);
             })
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (counter === 3) {
+            setCounterDisabled(true);
+        }
+    }, [counter])
+
+    const resetCounter = () => {
+        setCounter(0);
+        setCounterDisabled(false);
+    }
 
     return (
         <> 
+        <button onClick={e => setCounter(counter + 1)} disabled={counterDisabled}>{counter}</button>
+        <button onClick={resetCounter}>Reset</button>
+        <hr/>
         <h2>Post List</h2>
         {
             posts.length > 0 ? (posts.map(p => <div key={p.id}>{p.title}</div>)) : (
