c949bcb7cd8907ef2cfcb363da74aacc6dfa24bb
sandrine <sandrinezocca@gmail.com>
2019-07-24 15:34:55 +0200

    Dispatcher des actions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,6 +8,10 @@ const calculatorReducer = (state, action) => {
             return { a: action.a, b: action.b, total: action.a * action.b, operation: 'MULT' };
         case 'DIVIDE':
             return { a: action.a, b: action.b, total: action.a / action.b, operation: 'DIV' };
+        case 'RESET':
+            return { a: 0, b: 0, operation: 'RESET' };
+        default:
+            return state;
     }
 }
 
@@ -19,16 +23,32 @@ const Calculator = () => {
 
     const changeA = (e) => {
         setA(Number(e.target.value));
-    }
+    };
 
     const changeB = (e) => {
         setB(Number(e.target.value));
-    }
+    };
+
+    const reset = (e) => {
+        setA(0);
+        setB(0);
+        dispatch({type: 'RESET'});
+    };
 
     return ( 
         <div>
-            <input type="text" value={a} onChange={changeA}/>
-            <input type="text" value={b} onChange={changeB}/>
+            <input type="text" value={a} onChange={changeA}/><br/>
+            <input type="text" value={b} onChange={changeB}/><br/>
+            <button onClick={() => dispatch({ type: 'ADD', a:a, b:b})}>+</button>
+            <button onClick={() => dispatch({ type: 'MULTIPLY', a:a, b:b})}>*</button>
+            <button onClick={() => dispatch({ type: 'DIVIDE', a:a, b:b})}>/</button>
+            <button onClick={reset}>reset</button><br/>
+            {state.total}
+            <div>
+                <code>
+                    {JSON.stringify(state, null, 4)}
+                </code>
+            </div>
         </div>
      );
 }
