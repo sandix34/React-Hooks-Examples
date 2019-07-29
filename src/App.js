906131d747881890717f5a9e8da8134573c25b35
sandrine <sandrinezocca@gmail.com>
2019-07-29 17:39:57 +0200

    install antd

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from 'react';
-import Like from './components/Like';
-import FrameworkList from './components/FrameworkList';
-import PostList from './components/PostList';
-import Calculator from './components/Calculator';
-import LevelA from './components/LevelA';
-import MailContextProvider from './contexts/MailContext';
-import Keycode from './components/Keycode';
-import Login from './components/Login';
+import React from "react";
+import Like from "./components/Like";
+import FrameworkList from "./components/FrameworkList";
+import PostList from "./components/PostList";
+import Calculator from "./components/Calculator";
+import LevelA from "./components/LevelA";
+import MailContextProvider from "./contexts/MailContext";
+import Keycode from "./components/Keycode";
+import Login from "./components/Login";
+
+import { Collapse } from "antd";
+
+const { Panel } = Collapse;
+
+function callback(key) {
+  console.log(key);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Like/>
-        <FrameworkList/>
-        <PostList/>
-        <Calculator/>
-        <MailContextProvider>
-          <LevelA/>
-        </MailContextProvider>
-        <Keycode/>
-        <Login/>
-        
-      </header>
+        <Collapse defaultActiveKey={["1"]} onChange={callback}>
+          <Panel header="This is panel header 1" key="1">
+            <Like />
+          </Panel>
+          <Panel header="This is panel header 2" key="2">
+            <FrameworkList />
+          </Panel>
+          <Panel header="This is panel header 3" key="3">
+            <PostList />
+          </Panel>
+          <Panel header="This is panel header 4" key="4">
+            <Calculator />
+          </Panel>
+          <Panel header="This is panel header 5" key="5">
+            <MailContextProvider>
+              <LevelA />
+            </MailContextProvider>
+          </Panel>
+          <Panel header="This is panel header 6" key="6">
+            <Keycode />
+          </Panel>
+          <Panel header="This is panel header 7" key="7">
+            <Login />
+          </Panel>
+        </Collapse>
     </div>
   );
 }
--- src/components/Like.js
+++ src/components/Like.js
@@ -1,18 +1,26 @@
 // les hooks sont toujours préfixés de 'use...'
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Row, Col } from "antd";
 
 const Like = () => {
+  // Pour pouvoir gérer l'état on créer 2 variables dont la seconde est une fonction associée
+  // à la première qui permet de changer l'état
+  let [nbIfLikes, setNbOfLikes] = useState(0); // valeur initiale
 
-    // Pour pouvoir gérer l'état on créer 2 variables dont la seconde est une fonction associée 
-    // à la première qui permet de changer l'état
-    let [nbIfLikes, setNbOfLikes] = useState(0); // valeur initiale
+  return (
+    <div>
+      <Row type="flex" justify="space-around" align="middle">
+        <Col span={4}>
+          {nbIfLikes}
+          <button className="button-like" onClick={() => setNbOfLikes(nbIfLikes + 1)}>
+            Likes
+          </button>
+        </Col>
+            ici le code
+        <Col span={4} />
+      </Row>
+    </div>
+  );
+};
 
-    return ( 
-        <div>
-            {nbIfLikes}
-            <button onClick={() => setNbOfLikes(nbIfLikes + 1)}>Like</button>
-        </div>
-     );
-}
- 
 export default Like;
