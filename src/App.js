5e5518097b6372c93d379364c247b00af2de5aad
sandrine <sandrinezocca@gmail.com>
2019-07-31 16:15:09 +0200

    ajout code useContext()

--- src/App.js
+++ src/App.js
@@ -25,28 +25,33 @@ function App() {
   return (
     <div>
       <Layout>
-        <Header style={{"color": "white"}}>
-          React Hooks Examples 
+        <Header>
+          <h1 style={{"color": "white"}}>React Hooks Examples</h1> 
         </Header>
-        <Content>
-        <Collapse defaultActiveKey={["1"]} onChange={callback}>
+        <Content style={{"margin": "1rem"}}>
+        <Collapse style={{"padding": "1rem"}} defaultActiveKey={["1"]} onChange={callback}>
+          <h3>Gestion de l'état avec useState()</h3>
           <Panel header="Utiliser le hook useState() pour incrémenter" key="1">
             <Like />
           </Panel>
           <Panel forceRender="true" header="Utiliser plusieurs useState() dans un component -- Afficher la valeur d'un input" key="2">
             <FrameworkList />
           </Panel>
+          <h3>Gestion des side effects avec useEffect()</h3>
           <Panel forceRender="true" header="Utiliser useEffect() pour effectuer une requête AJAX -- Itérer les blog posts -- Afficher un indicateur de loading -- 1 useEffect() par fonctionnalité " key="3">
             <PostList />
           </Panel>
+          <h3>Gestion de l'état à l'aide de useReducer()</h3>
           <Panel forceRender="true" header="Création d'un component et de son reducer -- Dispatcher des actions" key="4">
             <Calculator />
           </Panel>
-          <Panel forceRender="true" header="This is panel header 5" key="5">
+          <h3>Consommation de contexts à l'aide de useContext()</h3>
+          <Panel forceRender="true" header="useContext() pour lire des messages depuis un component grand-parent -- Passer des données via 'context' -- Utiliser une fonction provenant du 'context'" key="5">
             <MailContextProvider>
               <LevelA />
             </MailContextProvider>
           </Panel>
+          <h3>Interraction avec le DOM et useRef()</h3>
           <Panel forceRender="true" header="This is panel header 6" key="6">
             <Keycode />
           </Panel>
--- src/components/LevelA.js
+++ src/components/LevelA.js
@@ -1,13 +1,159 @@
-import React from 'react';
-import LevelB from './LevelB';
+import React from "react";
+import LevelB from "./LevelB";
+
+import Prism from "prismjs";
+import "../prism.css";
+import { Row, Col } from "antd";
+
+import { Collapse } from "antd";
+const { Panel } = Collapse;
+
+function callback(key) {
+  console.log(key);
+}
 
 const LevelA = () => {
-    return ( 
+  return (
+    <Row type="flex" justify="space-around" align="middle">
+      <Col span={4}>
         <div className="level-a">
-            <h1>Level A</h1>
-            <LevelB/>
+          <h1>Level A</h1>
+          <LevelB />
         </div>
-     );
-}
- 
+      </Col>
+      <Col span={12}>
+        <Collapse onChange={callback}>
+          <Panel forceRender="true" header="Component Level A" key="1">
+            <pre>
+              <code className="language-javascript">
+                {`
+                  import React from 'react';
+                  import LevelB from './LevelB';
+                  
+                  const LevelA = () => {
+                      return ( 
+                          <div className="level-a">
+                              <h1>Level A</h1>
+                              <LevelB/>
+                          </div>
+                       );
+                  }
+                  
+                  export default LevelA;   
+                `}
+              </code>
+            </pre>
+          </Panel>
+          <Panel forceRender="true" header=" ComponentLevel B" key="2">
+            <pre>
+              <code className="language-javascript">
+                {`
+                    import React from 'react';
+                    import LevelC from './LevelC';
+                    
+                    const LevelB = () => {
+                        return ( 
+                            <div className="level-b">
+                                <h1>Level B</h1>
+                                <LevelC/>
+                            </div>
+                         );
+                    }
+                    
+                    export default LevelB; 
+                    `}
+              </code>
+            </pre>
+          </Panel>
+          <Panel forceRender="true" header="Component Level C" key="3">
+            <pre>
+              <code className="language-javascript">
+                {`
+                  import React, { useContext } from 'react';
+                  import { MailContext } from '../contexts/MailContext'
+                  
+                  const LevelC = () => {
+                  
+                      const {messages, deleteMessage} = useContext(MailContext);
+                  
+                      return ( 
+                          <div className="level-c">
+                              <h1>Level C</h1>
+                              <div>
+                                  Level A directly passed {messages.length} messages
+                                  <hr/>
+                                  {
+                                      messages.length > 0 ? (
+                                          <ul>
+                                          {messages.map(msg => <li key={msg.id}>{msg.title} <button onClick={() => deleteMessage(msg)}>delete</button></li>)}
+                                          </ul>
+                                      ) : (
+                                          <h3>no messages</h3>
+                                      )
+                                  }
+                              </div>
+                          </div>
+                       );
+                  }
+                   
+                  export default LevelC;  
+                `}
+              </code>
+            </pre>
+          </Panel>
+          <Panel forceRender="true" header="MailContext.js" key="4">
+            <pre>
+              <code className="language-javascript">
+                {`
+                  import React, { createContext, useState } from 'react';
+                  
+                  export const MailContext = createContext();
+                  
+                  const MailContextProvider = (props) => {
+                  
+                      const [messages, setMessages] = useState([
+                          { id: 1, title: "Birthday party", content: "Come and bring a bottle"},
+                          { id: 2, title: "This is a spam", content: "We want to sell you something"}
+                      ]);
+                  
+                      return ( 
+                        <MailContext.Provider value={{messages: messages, deleteMessage: deleteMessage}}>
+                              {props.children}
+                          </MailContext.Provider>
+                       );
+                  }
+                  
+                  export default MailContextProvider;  
+                `}
+              </code>
+            </pre>
+          </Panel>
+          <Panel forceRender="true" header="App.js" key="5">
+            <pre>
+              <code className="language-javascript">
+                {`
+                  import React from 'react';
+                  import LevelA from './components/LevelA';
+                  import MailContextProvider from './contexts/MailContext';
+                  
+                  function App() {
+                    return (
+                      <div>
+                          <MailContextProvider>
+                            <LevelA/>
+                          </MailContextProvider>
+                      </div>
+                    );
+                  }
+                  export default App;  
+                `}
+              </code>
+            </pre>
+          </Panel>
+        </Collapse>
+      </Col>
+    </Row>
+  );
+};
+
 export default LevelA;
