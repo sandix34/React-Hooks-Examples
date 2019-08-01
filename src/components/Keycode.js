59e86efc980103d6d3c7d853cf53c559d83fe0f6
sandrine <sandrinezocca@gmail.com>
2019-08-01 15:38:53 +0200

    ajout code Keycode + Login

--- src/components/Keycode.js
+++ src/components/Keycode.js
@@ -1,34 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import Prism from "prismjs";
+import "../prism.css";
+import { Row, Col } from "antd";
 
 const Keycode = () => {
+  const [keycode, setKeycode] = useState("");
+  const [key, setKey] = useState("");
+  const getKeyCode = e => {
+    console.log(e);
+    setKeycode(e.keyCode);
+    setKey(e.key);
+  };
 
-    const [keycode, setKeycode] = useState('');
-    const [key, setKey] = useState('');
-    const getKeyCode = (e) => {
-        console.log(e);
-        setKeycode(e.keyCode);
-        setKey(e.key);
-        
-    }
+  useEffect(() => {
+    document.addEventListener("keyup", getKeyCode);
+    return () => document.removeEventListener("keyup");
+  }, []);
 
-    useEffect(() => {
-        document.addEventListener('keyup', getKeyCode);
-        return () => document.removeEventListener('keyup');
-    }, []);
+  return (
+    <div>
+      <Row type="flex" justify="space-around" align="middle">
+        <Col span={4}>
+          <input type="text" />
+          {key.length === 0 ? (
+            <div>press any keyboard key</div>
+          ) : (
+            <div>Key is "{key}"</div>
+          )}
+          <div className="key-code">{keycode}</div>
+        </Col>
+        <pre>
+            <code className="language-javascript">
+                {`
+                  import React, { useState, useEffect } from 'react';
 
-    return ( 
-        <>
-            <input type="text"/>
-            {
-                key.length === 0 ? (
-                    <div>press any keyboard key</div>
-                ) : (
-                    <div>Key is "{key}"</div>
-                )
-            }
-            <div className="key-code">{keycode}</div>
-        </>
-     );
-}
- 
+                  const Keycode = () => {
+                  
+                      const [keycode, setKeycode] = useState('');
+                      const [key, setKey] = useState('');
+                      const getKeyCode = (e) => {
+                          console.log(e);
+                          setKeycode(e.keyCode);
+                          setKey(e.key);
+                  
+                      }
+                  
+                      useEffect(() => {
+                          document.addEventListener('keyup', getKeyCode);
+                          return () => document.removeEventListener('keyup');
+                      }, []);
+                  
+                      return ( 
+                          <>
+                              <input type="text"/>
+                              {
+                                  key.length === 0 ? (
+                                      <div>press any keyboard key</div>
+                                  ) : (
+                                      <div>Key is "{key}"</div>
+                                  )
+                              }
+                              <div className="key-code">{keycode}</div>
+                          </>
+                       );
+                  }
+                  
+                  export default Keycode;  
+                `}
+            </code>
+        </pre>
+      </Row>
+    </div>
+  );
+};
+
 export default Keycode;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, useRef} from 'react';
+import Prism from "prismjs";
+import '../prism.css'
+import { Row, Col } from "antd";
 
 
 const Login = () => {
@@ -22,17 +25,67 @@ const Login = () => {
     }
 
     return ( 
-        <>
-            <input 
-            type="text" 
-            ref={loginRef} 
-            value={login} 
-            onChange={handleChange} 
-            onKeyUp={handleKeyUp} 
-            placeholder="your login" />
-            <br/>
-            {message}
-        </>
+        <div>
+            <Row type="flex" justify="space-around" align="middle">
+                <Col span={4}>
+                <input 
+                    type="text" 
+                    ref={loginRef} 
+                    value={login} 
+                    onChange={handleChange} 
+                    onKeyUp={handleKeyUp} 
+                    placeholder="your login" />
+                    <br/>
+                    {message}
+                </Col>
+                <pre >
+                    <code className="language-javascript">
+                        {`
+                        import React, { useState, useEffect, useRef} from 'react';
+
+
+                        const Login = () => {
+                            const [login, setLogin] = useState('');
+                            const [message, setMessage] = useState('');
+                            const loginRef = useRef();
+                        
+                            useEffect(() => {
+                                console.log(loginRef);
+                                loginRef.current.focus();
+                            },[]);
+                        
+                            const handleChange = e => {
+                                setLogin(e.target.value);
+                            }
+                        
+                            const handleKeyUp = e => {
+                                if (e.keyCode === 13 ) {
+                                    `}setMessage(`Trying to login ${`{login}`}`){`
+                                }
+                            }
+                        
+                            return ( 
+                                <>
+                                    <input 
+                                    type="text" 
+                                    ref={loginRef} 
+                                    value={login} 
+                                    onChange={handleChange} 
+                                    onKeyUp={handleKeyUp} 
+                                    placeholder="your login" />
+                                    <br/>
+                                    {message}
+                                </>
+                            );
+                        }
+                        
+                        export default Login;  
+                        `}
+                    </code>
+                </pre>
+        </Row>
+            
+        </div>
      );
 }
  
